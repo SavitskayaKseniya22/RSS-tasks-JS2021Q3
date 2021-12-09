9f571abc5143a6dfbd2cc4f5e3b2204d8662ffe7
SavitskayaKseniya22 <77901301+SavitskayaKseniya22@users.noreply.github.com>
2021-12-09 18:55:42 +0300

    feat: change link to News API

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from "./loader";
 
 class AppLoader extends Loader {
   constructor() {
-    super("https://newsapi.org/v2/", {
+    super("https://nodenews.herokuapp.com/", {
       apiKey: "4731d8760b9a4d0dbc5655c35005a37d",
     });
   }
